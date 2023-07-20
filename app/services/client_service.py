23012b73bd878f33d7e24006a4ab344bf6fc6e8c
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-07-19 20:06:55 -0400

    build[client_service]
    created the client service

---
+++ app/services/client_service.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from app.models.client import Client
+from app.schema.client_schema import ClientSchema
+from app.repositories.client_repository import ClientRepository
+from app.mappers.client_mapper import ClientMapper
+
+
+class ClientService:
+
+    def __init__(self, client_repository: ClientRepository):
+        self.client_repository = client_repository
+
+    def get_clients(self) -> List[ClientSchema]:
+        return self.client_repository.get_clients()
+
+    def get_client_by_id(self, client_id: int) -> ClientSchema:
+        return self.client_repository.get_client_by_id(client_id)
+
+    def create_client(self, client: ClientSchema) -> Client:
+        client_db = Client(**ClientMapper.to_db(client))
+        return self.client_repository.create_item(client_db)
+
+    def update_client(self, client_id: int, client: ClientSchema) -> Client:
+        return self.client_repository.update_client(client_id, client)
+
+    def delete_client(self, client_id: int) -> None:
+        return self.client_repository.delete_client(client_id)
