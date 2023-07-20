a02a09f24d8be2955b23e7ade20c446c7e94c1a5
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-07-19 20:52:18 -0400

    build[sale]
    created the sale sqlalchemy model

---
+++ app/models/sale.py
@@ -0,0 +1,16 @@
+import datetime
+
+from sqlalchemy import Column, Integer, String, Float, DATETIME
+from sqlalchemy.orm import Mapped, mapped_column, mapper
+from app.db.database import Base
+
+
+class Sale(Base):
+    __tablename__ = "sale"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    id_item: Mapped[int] = mapped_column(Integer)
+    id_client: Mapped[int] = mapped_column(Integer)
+    amount: Mapped[int] = mapped_column(Integer)
+    sale_date: Mapped[datetime] = mapped_column(DATETIME)
+
