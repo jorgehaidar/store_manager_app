acc3abeee7691890d4c0a9fc0d51f8491bfa5bcb
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-07-19 20:47:39 -0400

    fix[client]
    fixed the data type of the phone attribute

--- app/models/client.py
+++ app/models/client.py
@@ -2,13 +2,14 @@ from sqlalchemy import Column, Integer, String, Float, DATE
 from sqlalchemy.orm import Mapped, mapped_column, mapper
 from app.db.database import Base
 
+
 class Client(Base):
     __tablename__ = "client"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String)
     identity_card: Mapped[str] = mapped_column(String)
-    phone: Mapped[str] = mapped_column(str)
+    phone: Mapped[str] = mapped_column(String)
     credit_card: Mapped[str] = mapped_column(String)
     debt: Mapped[float] = mapped_column(Float)
 
