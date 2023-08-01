4136fa93e0d6777253fa004cad9a8663acaddff5
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-01 13:50:09 -0400

    fix[item]: changed tablename items -> item

--- app/models/item.py
+++ app/models/item.py
@@ -6,7 +6,7 @@ from app.schema.item_schema import ItemSchema
 
 
 class Item(Base):
-    __tablename__ = "items"
+    __tablename__ = "item"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String)
