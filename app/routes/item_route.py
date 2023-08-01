e14468f26cfab88b051b7d77c9fcf0d80c2d6363
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-01 00:54:28 -0400

    refactor[item_route]: restructured the read_items method

--- app/routes/item_route.py
+++ app/routes/item_route.py
@@ -4,33 +4,35 @@ from fastapi import APIRouter, Depends
 from app.schema.item_schema import ItemSchema
 from app.repositories.item_repository import ItemRepository
 from app.services.item_service import ItemService
+from sqlalchemy.orm import Session
+from app.db.database import get_db
 
 router = APIRouter()
 
-item_service = ItemService(ItemRepository)
-
 
 @router.get("/items")
-async def read_items():
+async def read_items(db: Session = Depends(get_db)):
+    item_repository = ItemRepository(db)
+    item_service = ItemService(item_repository)
     return await item_service.get_items()
 
 
-@router.get("/items/{item_id}", response_model=ItemSchema)
-async def read_item(item_id: int, item_repository: ItemRepository = Depends()):
-    return item_repository.get_item_by_id(item_id=item_id)
-
-
-@router.post("/items", response_model=ItemSchema)
-async def create_item(item: ItemSchema, item_repository: ItemRepository = Depends()):
-    return item_repository.create_item(item=item)
-
-
-@router.put("/items/{item_id}", response_model=ItemSchema)
-async def update_item(item_id: int, item: ItemSchema, item_repository: ItemRepository = Depends()):
-    return item_repository.update_item(item_id=item_id, item=item)
-
-
-@router.delete("/items/{item_id}")
-async def delete_item(item_id: int, item_repository: ItemRepository = Depends()):
-    item_repository.delete_item(item_id=item_id)
-    return {"message": "Item deleted successfully."}
+# @router.get("/items/{item_id}", response_model=ItemSchema)
+# async def read_item(item_id: int, item_repository: ItemRepository = Depends()):
+#     return item_repository.get_item_by_id(item_id=item_id)
+#
+#
+# @router.post("/items", response_model=ItemSchema)
+# async def create_item(item: ItemSchema, item_repository: ItemRepository = Depends()):
+#     return item_repository.create_item(item=item)
+#
+#
+# @router.put("/items/{item_id}", response_model=ItemSchema)
+# async def update_item(item_id: int, item: ItemSchema, item_repository: ItemRepository = Depends()):
+#     return item_repository.update_item(item_id=item_id, item=item)
+#
+#
+# @router.delete("/items/{item_id}")
+# async def delete_item(item_id: int, item_repository: ItemRepository = Depends()):
+#     item_repository.delete_item(item_id=item_id)
+#     return {"message": "Item deleted successfully."}
