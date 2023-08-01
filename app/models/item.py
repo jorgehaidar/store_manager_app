5290adfe85e278cfad23540421d80820d8986037
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-01 11:33:36 -0400

    feat[item]: added relation item to sale

--- app/models/item.py
+++ app/models/item.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, Float, DATE
-from sqlalchemy.orm import Mapped, mapped_column, mapper
+from sqlalchemy.orm import Mapped, mapped_column, mapper, relationship
 from app.db.database import Base
 from app.schema.item_schema import ItemSchema
 
@@ -16,6 +16,7 @@ class Item(Base):
     tax: Mapped[float] = mapped_column(Float)
     location: Mapped[str] = mapped_column(String)
     #expiration_date: Mapped[datetime] = mapped_column(DATE)
+    sale = relationship('Sale', uselist=False)
 
     # TODO: Add the column purchase_date
     # TODO: Add the column expiration_date
