15983284e1eea495b79f77d91ec6362af9d05014
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-01 13:20:44 -0400

    feat[main]: added router client_route and sale_route

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import uvicorn
 from fastapi import FastAPI
 from app.db.database import Base, engine, SessionLocal
 from app.routes import item_route
+from app.routes import client_route
+from app.routes import sale_route
 
 from app.models.item import Item
 from app.schema.item_schema import ItemSchema
 
 app = FastAPI()
 app.include_router(item_route.router)
-# TODO: add client api routes
-# TODO: add sale api routes
+app.include_router(client_route.router)
+app.include_router(sale_route.router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
