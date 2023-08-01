4a9a6acb9e62cfb3f4f6e048a1fd1d5979b47216
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-01 11:38:26 -0400

    refactor[client_route]: restructured read_clients method

--- app/routes/client_route.py
+++ app/routes/client_route.py
@@ -4,13 +4,17 @@ from fastapi import APIRouter, Depends
 from app.schema.client_schema import ClientSchema
 from app.repositories.client_repository import ClientRepository
 from app.services.client_service import ClientService
+from app.db.database import get_db
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
 
 @router.get("/clients")
-async def read_clients(client_repository: ClientRepository = Depends()):
-    return await client_repository.get_clients()
+async def read_clients(db: Session = Depends(get_db)):
+    client_repository = ClientRepository(db)
+    client_service = ClientService(client_repository)
+    return await client_service.get_clients()
 
 
 @router.get("/clients/{client_id}", response_model=ClientSchema)
