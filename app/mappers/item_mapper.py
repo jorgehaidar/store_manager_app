e6b6cafb6cc032a6e213ac31d04b129f3d78179b
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-01 03:18:30 -0400

    feat[item_mapper]: added to transform item_db to item_schema

--- app/mappers/item_mapper.py
+++ app/mappers/item_mapper.py
@@ -9,5 +9,14 @@ class ItemMapper:
         return item.dict()
 
     @staticmethod
-    def to_entity(item: dict) -> ItemSchema:
-        return ItemSchema(**item)
+    def to_entity(item: Item) -> ItemSchema:
+        item_dict = {
+            'id': item.id,
+            'name': item.name,
+            'price': item.price,
+            'purchase_price': item.purchase_price,
+            'tax': item.tax,
+            'location': item.location
+        }
+        return ItemSchema(**item_dict)
+
