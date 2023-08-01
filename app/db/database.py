76c243f8d7d9b0dda1ba945f650a9eb3bffad4b7
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-01 00:21:36 -0400

    build[database]: created get_db() dependency

--- app/db/database.py
+++ app/db/database.py
@@ -6,3 +6,11 @@ SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5432/store_mana
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(engine)
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
