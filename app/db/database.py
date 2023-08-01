397fc0f6a7222fa2809376acb7f1232664fc3d39
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-01 13:49:25 -0400

    fix[database]: changed database store_manager_db -> store_manager_db2

--- app/db/database.py
+++ app/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5432/store_manager_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5432/store_manager_db2"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(engine)
 Base = declarative_base()
