5ae898ca9e8682d91c74724f31397ed0cb273027
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-03 15:13:53 -0400

    feat[item_repository]: added status code

--- app/routes/item_route.py
+++ app/routes/item_route.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Any
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from app.schema.item_schema import ItemSchema
@@ -26,37 +26,35 @@ async def read_item(item_id: int, db: Session = Depends(get_db)):
     item_service = ItemService(item_repository)
     item = item_service.get_item_by_id(item_id=item_id)
     if item is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail='Item not found'
-        )
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Item with id {item_id} is not available')
     return item
 
-#TODO: El estado de respuesta de creado debe ser 201
-@router.post("/")
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_item(item: ItemSchema, db: Session = Depends(get_db)):
     item_repository = ItemRepository(db)
     item_service = ItemService(item_repository)
     return item_service.create_item(item=item)
 
 
-@router.put("/{item_id}")
+@router.put("/{item_id}", status_code=status.HTTP_200_OK)
 async def update_item(item_id: int, item: ItemSchema, db: Session = Depends(get_db)):
     item_repository = ItemRepository(db)
     item_service = ItemService(item_repository)
-    return item_service.update_item(item_id=item_id, item=item)
+    item = item_service.update_item(item_id=item_id, item=item)
+    if item is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Item with id {item_id} is not available')
+    return item
 
 
-@router.delete("/{item_id}")
+@router.delete("/{item_id}", status_code=status.HTTP_200_OK)
 async def delete_item(item_id: int, db: Session = Depends(get_db)):
     item_repository = ItemRepository(db)
     item_service = ItemService(item_repository)
     item = item_service.delete_item(item_id=item_id)
     if item:
-        return {"message": "Item deleted successfully."}
-    else:
-        raise HTTPException(
-            status_code=404,
-            detail='Item not found'
-        )
-
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Item with id {item_id} is not available')
+    return {"message": "Item deleted successfully."}
