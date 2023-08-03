4b15d34ec83271b01eae0a73bf4d78f446f392fb
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-03 16:56:45 -0400

    build[user_mapper]: created the user mapper

---
+++ app/mappers/user_mapper.py
@@ -0,0 +1,19 @@
+from app.models.user import User
+from app.schema.user_schema import UserSchema
+
+
+class UserMapper:
+
+    @staticmethod
+    def to_db(user: UserSchema) -> dict:
+        return user.dict()
+
+    @staticmethod
+    def to_entity(user: User) -> UserSchema:
+        user_dict = {
+            'id': user.id,
+            'email': user.email,
+            'password': user.password,
+        }
+        return UserSchema(**user_dict)
+
