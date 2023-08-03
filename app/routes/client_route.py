86e84df8685dc8c6fb329c811202f0329b2fffb9
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-03 12:36:36 -0400

    refactor[client_route]: added prefix and tags to router and refactor endpoint

--- app/routes/client_route.py
+++ app/routes/client_route.py
@@ -1,44 +1,53 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.schema.client_schema import ClientSchema
 from app.repositories.client_repository import ClientRepository
 from app.services.client_service import ClientService
 from app.db.database import get_db
 from sqlalchemy.orm import Session
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/clients',
+    tags=['clients']
+)
 
 
-@router.get("/clients")
+@router.get("/")
 async def read_clients(db: Session = Depends(get_db)):
     client_repository = ClientRepository(db)
     client_service = ClientService(client_repository)
     return await client_service.get_clients()
 
 
-@router.get("/clients/{client_id}")
+@router.get("/{client_id}")
 async def read_client(client_id: int, db: Session = Depends(get_db)):
     client_repository = ClientRepository(db)
     client_service = ClientService(client_repository)
-    return client_service.get_client_by_id(client_id=client_id)
+    client = client_service.get_client_by_id(client_id=client_id)
+    if client is None:
+        raise HTTPException(
+            status_code=404,
+            detail='Client not found'
+        )
+    return client
 
-
-@router.post("/clients")
+#TODO: El estado de respuesta de creado debe ser 201
+@router.post("/")
 async def create_client(client: ClientSchema, db: Session = Depends(get_db)):
     client_repository = ClientRepository(db)
     client_service = ClientService(client_repository)
     return client_service.create_client(client=client)
 
 
-@router.put("/clients/{client_id}", response_model=ClientSchema)
+@router.put("/{client_id}", response_model=ClientSchema)
 async def update_client(client_id: int, client: ClientSchema, db: Session = Depends(get_db)):
     client_repository = ClientRepository(db)
     client_service = ClientService(client_repository)
     return client_service.update_client(client_id=client_id, client=client)
 
 
-@router.delete("/clients/{client_id}")
+@router.delete("/{client_id}")
 async def delete_client(client_id: int, db: Session = Depends(get_db)):
     client_repository = ClientRepository(db)
     client_service = ClientService(client_repository)
