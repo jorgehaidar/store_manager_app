29fb1a18891acba20d200462cddc153e92306ae0
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-03 15:06:18 -0400

    chore[item_repository]: minor changes in doc, comments and return type

--- app/repositories/item_repository.py
+++ app/repositories/item_repository.py
@@ -1,8 +1,5 @@
 from typing import List
-from app.db.database import SessionLocal
 from app.models.item import Item
-from app.schema.item_schema import ItemSchema
-from app.mappers.item_mapper import ItemMapper
 from sqlalchemy.orm import Session
 
 
@@ -10,28 +7,19 @@ class ItemRepository:
     def __init__(self, db: Session):
         self.db = db
 
-    async def get_items(self) -> List:
+    async def get_items(self) -> List[Item]:
         return self.db.query(Item).all()
-        #with SessionLocal() as session:
-        #    return session.query(Item).all()
 
-    def get_item_by_id(self, item_id: int):
+    def get_item_by_id(self, item_id: int) -> Item:
         return self.db.query(Item).filter(Item.id == item_id).first()
 
-    def create_item(self, item: ItemSchema):
-        #TODO: arreglar el atributo que recibe ItemSchema -> Item
+    def create_item(self, item: Item) -> Item:
         self.db.add(item)
         self.db.commit()
         self.db.refresh(item)
         return item
-        # with SessionLocal() as session:
-        #     item_db = Item(**ItemMapper.to_db(item))
-        #     session.add(item_db)
-        #     session.commit()
-        #     session.refresh(item_db)
-        #     return item
 
-    def update_item(self, item_id: int, item: Item):
+    def update_item(self, item_id: int, item: Item) -> Item:
         db_item: Item = self.db.query(Item).filter(Item.id == item_id).first()
 
         if item.name != None:
@@ -53,9 +41,7 @@ class ItemRepository:
         self.db.refresh(db_item)
         return db_item
 
-    def delete_item(self, item_id: int) -> None:
-        self.db.query(Item).filter(Item.id == item_id).delete()
+    def delete_item(self, item_id: int):
+        delete = self.db.query(Item).filter(Item.id == item_id).delete()
         self.db.commit()
-        # with SessionLocal() as session:
-        #     session.query(Item).filter(Item.id == item_id).delete()
-        #     session.commit()
+        return delete
