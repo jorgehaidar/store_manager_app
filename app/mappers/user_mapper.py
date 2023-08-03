ef148f08b597c65ccde991d75dfbdaccf29e4661
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-03 17:57:37 -0400

    build[user_mapper]: created the atribute name

--- app/mappers/user_mapper.py
+++ app/mappers/user_mapper.py
@@ -13,6 +13,7 @@ class UserMapper:
         user_dict = {
             'id': user.id,
             'email': user.email,
+            'name': user.name,
             'password': user.password,
         }
         return UserSchema(**user_dict)
