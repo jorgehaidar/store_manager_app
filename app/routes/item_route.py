984ca8aff80a34f7c73960ef0f89f36c00a4272e
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-08 11:41:48 -0400

    feat[item_route]: added authentication to endpoints

--- app/routes/item_route.py
+++ app/routes/item_route.py
@@ -6,6 +6,8 @@ from app.repositories.item_repository import ItemRepository
 from app.services.item_service import ItemService
 from sqlalchemy.orm import Session
 from app.db.database import get_db
+from app.schema.user_schema import UserSchema
+from .. import oaut2
 
 router = APIRouter(
     prefix='/items',
@@ -14,14 +16,16 @@ router = APIRouter(
 
 
 @router.get("/")
-async def read_items(db: Session = Depends(get_db)):
+async def read_items(db: Session = Depends(get_db), current_user: UserSchema = Depends(oaut2.get_current_user)):
     item_repository = ItemRepository(db)
     item_service = ItemService(item_repository)
     return await item_service.get_items()
 
 
 @router.get("/{item_id}", status_code=status.HTTP_200_OK)
-async def read_item(item_id: int, db: Session = Depends(get_db)):
+async def read_item(item_id: int,
+                    db: Session = Depends(get_db),
+                    current_user: UserSchema = Depends(oaut2.get_current_user)):
     item_repository = ItemRepository(db)
     item_service = ItemService(item_repository)
     item = item_service.get_item_by_id(item_id=item_id)
@@ -32,14 +36,19 @@ async def read_item(item_id: int, db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_item(item: ItemSchema, db: Session = Depends(get_db)):
+async def create_item(item: ItemSchema,
+                      db: Session = Depends(get_db),
+                      current_user: UserSchema = Depends(oaut2.get_current_user)):
     item_repository = ItemRepository(db)
     item_service = ItemService(item_repository)
     return item_service.create_item(item=item)
 
 
 @router.put("/{item_id}", status_code=status.HTTP_200_OK)
-async def update_item(item_id: int, item: ItemSchema, db: Session = Depends(get_db)):
+async def update_item(item_id: int,
+                      item: ItemSchema,
+                      db: Session = Depends(get_db),
+                      current_user: UserSchema = Depends(oaut2.get_current_user)):
     item_repository = ItemRepository(db)
     item_service = ItemService(item_repository)
     item = item_service.update_item(item_id=item_id, item=item)
@@ -50,7 +59,9 @@ async def update_item(item_id: int, item: ItemSchema, db: Session = Depends(get_
 
 
 @router.delete("/{item_id}", status_code=status.HTTP_200_OK)
-async def delete_item(item_id: int, db: Session = Depends(get_db)):
+async def delete_item(item_id: int,
+                      db: Session = Depends(get_db),
+                      current_user: UserSchema = Depends(oaut2.get_current_user)):
     item_repository = ItemRepository(db)
     item_service = ItemService(item_repository)
     item = item_service.delete_item(item_id=item_id)
