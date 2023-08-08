54ba93a10603907b2c52d4e8b29bd5dcbc1c08e7
Jorge Haidar Martinez <jorgehaidarmtnez@gmail.com>
2023-08-08 11:44:23 -0400

    feat[main]: added new routes

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
-from app.db.database import Base, engine, SessionLocal
-from app.routes import item_route
-from app.routes import client_route
-from app.routes import sale_route
-
-from app.models.item import Item
-from app.schema.item_schema import ItemSchema
+from app.routes import item_route, client_route, sale_route, user_route, authentication
 
 app = FastAPI()
+app.include_router(authentication.router)
 app.include_router(item_route.router)
 app.include_router(client_route.router)
 app.include_router(sale_route.router)
+app.include_router(user_route.router)
+
 
 if __name__ == '__main__':
+    # To Run
     uvicorn.run('main:app', reload=True)
-    #Base.metadata.drop_all(engine)
-    #Base.metadata.create_all(engine)
+
+    # To DEBUG
+    # uvicorn.run(app, host='127.0.0.1', port=8081)
+
+    # Base.metadata.drop_all(engine)
+    # Base.metadata.create_all(engine)
 
